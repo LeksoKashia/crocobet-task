80f4d81ef0cd53a793b020b4c6ccd02e9a2e91b4
Lekso Kashia <lekso.kashia.1@btu.edu.ge>
2024-07-11 11:05:38 +0400

    implemented functioning code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { NavigationComponent } from './core/components/navigation/navigation.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,NavigationComponent, MatTabsModule,MatButtonModule , MatSidenavModule, MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'crocobet-task';
+  collapsed = signal(true);
+  sidenavWidth = computed(() => this.collapsed() ? '70px': '250px')
 }
