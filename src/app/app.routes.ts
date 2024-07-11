961f2f4eae9714b12e2c4010374f6954f05cf801
Lekso Kashia <lekso.kashia.1@btu.edu.ge>
2024-07-11 11:03:03 +0400

    created routes and implemented lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: "home"},
+    {
+      path: 'home',
+      loadComponent: () =>
+        import('./core/components/home/home.component').then((m) => m.HomeComponent),
+    },
+    {
+      path: 'users',
+      loadComponent: () =>
+        import('./core/components/user-list/user-list.component').then((m) => m.UserListComponent),
+    },
+    {
+      path: 'posts',
+      loadComponent: () =>
+        import('./core/components/post-list/post-list.component').then((m) => m.PostListComponent),
+    }
+  ];
