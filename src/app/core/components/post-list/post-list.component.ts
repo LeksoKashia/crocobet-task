daf3d115ce82f0cfa4e04864f391eccf26c7e4fd
Lekso Kashia <lekso.kashia.1@btu.edu.ge>
2024-07-11 17:10:34 +0400

    implemented post list logic

--- src/app/core/components/post-list/post-list.component.ts
+++ src/app/core/components/post-list/post-list.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { DynamicTableComponent } from '../../../shared/dynamic-table/dynamic-table.component';
+import { forkJoin, map, mergeMap, Observable } from 'rxjs';
+import { UserDataService } from '../../services/user-data.service';
 
 @Component({
   selector: 'post-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DynamicTableComponent],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostListComponent {
+  displayedColumns: string[] = ['username', 'title', 'details'];
+  dataSource = new MatTableDataSource<any>([]);
+  posts$: Observable<any[]>;
 
+  constructor(private userDataService: UserDataService) {}
+
+  ngOnInit(): void {
+    this.posts$ = this.userDataService.fetchData();
+    this.posts$.subscribe((posts) => {
+      this.dataSource.data = posts;
+    });
+  }
 }
