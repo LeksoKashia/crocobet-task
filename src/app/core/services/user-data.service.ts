0b6561572d8eaecf728afaddc3a5ac48fbcb87d3
Lekso Kashia <lekso.kashia.1@btu.edu.ge>
2024-07-13 15:17:01 +0400

    added user related requests

--- src/app/core/services/user-data.service.ts
+++ src/app/core/services/user-data.service.ts
@@ -3,39 +3,45 @@ import { Injectable } from '@angular/core';
 import { forkJoin, map, Observable } from 'rxjs';
 import { User } from '../models/user.model.';
 import { Post } from '../models/post.model';
+import { environment } from '../../../environments/environment';
+import { AppUrlEnum } from '../../const/route.enums';
+import { Todo } from '../models/todo.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserDataService {
-  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
-  private postsUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[] | any> {
-    return this.http.get<User[]>(this.usersUrl).pipe(
-      map(users => {
-        return users.map(user => ({
-          ...user,
-          company: user.company.name
-        }));
-      })
-    );
+    return this.http
+      .get<User[]>(`${environment.baseUrl_api}/${AppUrlEnum.USERS}`)
+      .pipe(
+        map((users) => {
+          return users.map((user) => ({
+            ...user,
+            company: user.company.name,
+          }));
+        })
+      );
   }
 
   getPosts(): Observable<Post[] | any> {
     return forkJoin({
-      users: this.http.get<User[]>(this.usersUrl),
-      posts: this.http.get<Post[]>(this.postsUrl)
+      users: this.http.get<User[]>(
+        `${environment.baseUrl_api}/${AppUrlEnum.USERS}`
+      ),
+      posts: this.http.get<Post[]>(
+        `${environment.baseUrl_api}/${AppUrlEnum.POSTS}`
+      ),
     }).pipe(
       map(({ users, posts }) => {
         const userMap = new Map<number, string>();
-        users.forEach(user => {
+        users.forEach((user) => {
           userMap.set(user.id, user.name);
         });
 
-        posts.forEach(post => {
+        posts.forEach((post) => {
           post.username = userMap.get(post.userId) || 'Unknown User';
         });
         return posts;
@@ -43,4 +49,19 @@ export class UserDataService {
     );
   }
 
+  getUser(userId: number): Observable<User> {
+    return this.http.get<User>(`${environment.baseUrl_api}/users/${userId}`);
+  }
+
+  getUserPosts(userId: number): Observable<Post[]> {
+    return this.http.get<Post[]>(
+      `${environment.baseUrl_api}/posts?userId=${userId}`
+    );
+  }
+
+  getUserTodos(userId: number): Observable<Todo[]> {
+    return this.http.get<Todo[]>(
+      `${environment.baseUrl_api}/todos?userId=${userId}`
+    );
+  }
 }
