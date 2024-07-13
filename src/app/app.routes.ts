e8795fdbaafd7d0c7ab48c6a91014d7997b02ead
Lekso Kashia <lekso.kashia.1@btu.edu.ge>
2024-07-13 15:17:45 +0400

    modified routes implemented children loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,43 +2,19 @@ import { Routes } from '@angular/router';
 import { AppUrlEnum } from './const/route.enums';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: `${AppUrlEnum.HOME}` },
-  {
-    path: AppUrlEnum.HOME,
-    loadComponent: () =>
-      import('./core/components/home/home.component').then(
-        (m) => m.HomeComponent
-      ),
-  },
+  { path: '', pathMatch: 'full', redirectTo: `${AppUrlEnum.USERS}` },
   {
     path: AppUrlEnum.USERS,
-    loadComponent: () =>
-      import('./core/components/user-list/user-list.component').then(
-        (m) => m.UserListComponent
-      ),
+    loadChildren: () =>
+      import('./pages/user-list/user.routes').then((r) => r.routes),
   },
   {
     path: AppUrlEnum.POSTS,
     loadComponent: () =>
-      import('./core/components/post-list/post-list.component').then(
+      import('./pages/post-list/post-list.component').then(
         (m) => m.PostListComponent
       ),
   },
 
-  {
-    path: `${AppUrlEnum.USERS}/${AppUrlEnum.POSTS}/:id`,
-    loadComponent: () =>
-      import(
-        './core/components/user-specific-posts/user-specific-posts.component'
-      ).then((m) => m.UserSpecificPostsComponent),
-  },
-
-  {
-    path: `${AppUrlEnum.USERS}/${AppUrlEnum.TODOS}/:id`,
-    loadComponent: () =>
-      import(
-        './core/components/user-specific-todos/user-specific-todos.component'
-      ).then((m) => m.UserSpecificTodosComponent),
-  },
-  { path: '**', redirectTo: `${AppUrlEnum.HOME}`, pathMatch: 'full' }
+  { path: '**', redirectTo: `${AppUrlEnum.USERS}`, pathMatch: 'full' },
 ];
